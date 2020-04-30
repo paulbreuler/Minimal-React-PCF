25e141790e30eef08a10e6f8778f7ff8b97574e3
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2020-04-30 15:39:18 -0400

    update custom button with simple onclick

--- minReactPcf/extension.tsx
+++ minReactPcf/extension.tsx
@@ -15,16 +15,41 @@ export class CustomButton extends React.Component<IButtonProps, IButtonState> {
     constructor(props: IButtonProps) {
         super(props);
         this.state = { exampleState: false }
-        this.alertClicked = this.alertClicked.bind(this);
+        this.getLocation = this.getLocation.bind(this);
     }
 
-    alertClicked(): void {
-        alert('Clicked');
+    async getLocation(): Promise<any> {
+        debugger;
+        var myHeaders = new Headers();
+
+        var requestOptions: RequestInit = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        try {
+            let response = await fetch(`https://api.weather.gov/points/41.4993,-81.6944`, requestOptions)
+
+            if (response.status !== 200) {
+                console.log(`${response.status} ${response.statusText} | ${response.url}`);
+                return;
+            }
+
+            let responsetext = await response.text();
+            let text = JSON.parse(responsetext);
+
+            alert(`Relative Location: ${text.properties.relativeLocation.properties.city}, ${text.properties.relativeLocation.properties.state}`);
+
+        } catch (e) {
+            throw e;
+        }
+
     }
 
-   render(){
-       return(
-        <DefaultButton text="test" />
-       )
-   }
+    render() {
+        return (
+            <DefaultButton text="Get Location Data" onClick={this.getLocation} />
+        )
+    }
 }
